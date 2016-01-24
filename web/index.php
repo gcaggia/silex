f3350b7a56b6a67a1ff6a0308e54953c0aca36c2
Guillaume Caggia <guillaume.caggia@gmail.com>
2016-01-24 17:08:31 +0100

    silex first app

---
+++ web/index.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app->get('/', function () {
+    return 'Hello world';
+});
+
+$app->get('/hello/{name}', function ($name) use ($app) {
+    return 'Hello ' . $app->escape($name);
+});
+
+$app->run();
